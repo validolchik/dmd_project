8c08f26a35d45b1924ca6f1baa225af01c1dc734
valik <valik.valeevrenat@mail.ru>
2019-11-27 14:01:32 +0300

    added more structure to code

--- filler.py
+++ filler.py
@@ -7,7 +7,7 @@ con = psycopg2.connect(
     user="postgres",
     password="postgres",
     host="localhost",
-    port="60118"
+    port="60631"
 )
 
 # http://localhost:60118
@@ -20,119 +20,159 @@ file_writing = False
 # cur.execute("DELETE FROM appointment;")
 # cur.execute("DELETE FROM person;")
 
+def write_into_file(string_query):
+    if file_writing:
+        file_output.write(string_query + "\n")
+
+
+# insert male into table person and return query and id of last added person
+def create_person_male():
+    ssn, address, name, surname, date_of_birth, sex = \
+        fake.itin(), fake.address(), fake.first_name_male(), \
+        fake.last_name_male(), fake.date_of_birth(tzinfo=None, minimum_age=15, maximum_age=115), 'm'
+
+    address = address.replace("\n", ", ")
+    query_person = "INSERT INTO person (ssn, address, name, surname, date_of_birth, sex) \
+                VALUES (%s, %s, %s, %s, %s, %s) RETURNING person_id;"
+    cur.execute(query_person, (ssn, address, name, surname, date_of_birth, sex))
+    query_person = query_person % (ssn, address, name, surname, date_of_birth, sex)
+    person_id = cur.fetchone()[0]
+    return [query_person, person_id]
+
+
+# insert female into table person and return query and id of last added person
+def create_person_female():
+    ssn, address, name, surname, date_of_birth, sex = \
+        fake.itin(), fake.address(), fake.first_name_female(), \
+        fake.last_name_female(), fake.date_of_birth(tzinfo=None, minimum_age=15, maximum_age=115), 'm'
+
+    address = address.replace("\n", ", ")
+    query_person = "INSERT INTO person (ssn, address, name, surname, date_of_birth, sex) \
+                VALUES (%s, %s, %s, %s, %s, %s) RETURNING person_id;"
+    cur.execute(query_person, (ssn, address, name, surname, date_of_birth, sex))
+    query_person = query_person % (ssn, address, name, surname, date_of_birth, sex)
+    person_id = cur.fetchone()[0]
+    return [query_person, person_id]
+
+
 # inserting patients
 def inserting_patients():
-    cur.execute("DELETE FROM patient;")
+    # cur.execute("DELETE FROM patient;")
     for i in range(25):
-        ssn, address, name, surname, date_of_birth, sex = \
-            fake.itin(), fake.address(), fake.first_name_male(), \
-            fake.last_name_male(), fake.date_of_birth(tzinfo=None, minimum_age=15, maximum_age=115), 'm'
+        query_id = create_person_male()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+        query_patient = "INSERT INTO patient (person_id) VALUES (%s);"
+        cur.execute(query_patient % person_id)
+        write_into_file(query_patient)
 
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO patient (ssn, address, name, surname, date_of_birth, sex) \
-            VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (ssn, address, name, surname, date_of_birth, sex)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
 
     for i in range(25):
-        ssn, address, name, surname, date_of_birth, sex = \
-            fake.itin(), fake.address(), fake.first_name_female(), \
-            fake.last_name_female(), fake.date_of_birth(tzinfo=None, minimum_age=15, maximum_age=115), 'f'
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO patient (ssn, address, name, surname, date_of_birth, sex) \
-            VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (ssn, address, name, surname, date_of_birth, sex)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+        query_patient = "INSERT INTO patient (person_id) VALUES (%s);"
+        cur.execute(query_patient % person_id)
+        write_into_file(query_patient)
 
 # inserting doctors
 def inserting_doctors():
-    cur.execute("DELETE FROM doctor;")
+    # cur.execute("DELETE FROM doctor;")
     for i in range(12):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_male(), fake.last_name_male(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'm', fake.random_int(min=3, max=25, step=1), \
-                                         fake.random_int(min=30000, max=45000, step=1000)
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO doctor (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-            VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_male()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+        
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=40000, max=80000, step=2000)
+        
+        query_doctor = "INSERT INTO doctor (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_doctor % (person_id,length_of_service, salary))
+        write_into_file(query_doctor)
 
     for i in range(5):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_female(), fake.last_name_female(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'f', fake.random_int(min=3, max=15, step=1), \
-                                         fake.random_int(min=30000, max=45000, step=1000),
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO doctor (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-                VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=40000, max=80000, step=2000)
+
+        query_doctor = "INSERT INTO doctor (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_doctor % (person_id, length_of_service, salary))
+        write_into_file(query_doctor)
+
 
 # inserting nurses
 def inserting_nurses():
-    cur.execute("DELETE FROM nurse;")
+    # cur.execute("DELETE FROM nurse;")
     for i in range(10):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_female(), fake.last_name_female(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'f', fake.random_int(min=3, max=15, step=1), \
-                                         fake.random_int(min=20000, max=30000, step=1000)
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO nurse (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-                    VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=20000, max=40000, step=2000)
+
+        query_nurse = "INSERT INTO nurse (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_nurse % (person_id, length_of_service, salary))
+        write_into_file(query_nurse)
 
 
 # inserting accountants
 def inserting_accountants():
-    cur.execute("DELETE FROM accountant;")
+    # cur.execute("DELETE FROM accountant;")
     for i in range(4):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_female(), fake.last_name_female(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'f', fake.random_int(min=1, max=5, step=1),\
-                                         fake.random_int(min=30000, max=40000, step=1000)
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO accountant (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-                        VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=20000, max=40000, step=2000)
+        
+        query_accountant = "INSERT INTO accountant (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_accountant % (person_id, length_of_service, salary))
+        write_into_file(query_accountant)
 
 # inserting receptionists
 def inserting_receptionists():
-    cur.execute("DELETE FROM receptionist;")
+    # cur.execute("DELETE FROM receptionist;")
     for i in range(4):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_female(), fake.last_name_female(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'f', fake.random_int(min=1, max=5, step=1),\
-                                         fake.random_int(min=30000, max=40000, step=1000)
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO receptionist (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-                            VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
 
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=20000, max=40000, step=2000)
+        
+        query_recept = "INSERT INTO receptionist (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_recept % (person_id, length_of_service, salary))
+        write_into_file(query_recept)
+
+
+# inserting storekeepers
+def inserting_storekeepers():
+    # cur.execute("DELETE FROM storekeeper;")
+    for i in range(2):
+        query_id = create_person_female()
+        query_person = query_id[0]
+        person_id = query_id[1]
+        write_into_file(query_person)
+        length_of_service = fake.random_int(min=3, max=20, step=1)
+        salary = fake.random_int(min=20000, max=40000, step=2000)
+        query_storekeeper = "INSERT INTO storekeeper (person_id, length_of_service, salary) VALUES (%s, %s, %s);"
+        cur.execute(query_storekeeper % (person_id, length_of_service, salary))
+        write_into_file(query_storekeeper)
 
 # inserting appointments
 def inserting_appointments():
-    cur.execute("DELETE FROM appointment;")
+    # cur.execute("DELETE FROM appointment;")
     for i in range(100):
         cur.execute("SELECT person_id FROM patient ORDER BY random() LIMIT 1;")
         patient_id = int(cur.fetchone()[0])
@@ -142,29 +182,10 @@ def inserting_appointments():
 
         time = fake.date_time_between(start_date="-1d", end_date="now", tzinfo=None)
 
-        query = "INSERT INTO appointment(app_time, room, patient_id, rec_id) VALUES ('%s', '%s', '%s', '%s');" % \
-                    (time, fake.random_int(min = 1, max = 30, step=1), patient_id, rec_id)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
-
-
-# inserting storekeepers
-def inserting_storekeepers():
-    cur.execute("DELETE FROM storekeeper;")
-    for i in range(2):
-        ssn, address, name, surname, date_of_birth = \
-            fake.itin(), fake.address(), fake.first_name_female(), fake.last_name_female(), \
-            fake.date_of_birth(tzinfo=None, minimum_age=27, maximum_age=60)
-        sex, length_of_service, salary = 'f', fake.random_int(min = 3, max = 15, step = 1), \
-                                         fake.random_int(min = 20000, max = 25000, step= 1000)
-        address = address.replace("\n", ", ")
-        query = "INSERT INTO storekeeper (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary) \
-                        		VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s');" % \
-                (ssn, address, name, surname, date_of_birth, sex, length_of_service, salary)
-        cur.execute(query)
-        if file_writing:
-            file_output.write(query + "\n")
+        query = "INSERT INTO appointment(app_time, room, patient_id, rec_id) VALUES ('%s', '%s', '%s', '%s');"
+        cur.execute(query, (time, fake.random_int(min = 1, max = 30, step=1), patient_id, rec_id))
+        query = query % (time, fake.random_int(min = 1, max = 30, step=1), patient_id, rec_id)
+        write_into_file(query)
 
 
 # inserting medic_appointments
@@ -236,16 +257,24 @@ def inserting_qualifications():
             if file_writing:
                 file_output.write(query + "\n")
 
-# inserting_patients()
-# inserting_doctors()
-# inserting_nurses()
-# inserting_accountants()
-# inserting_receptionists()
-# inserting_storekeepers()
-# inserting_medicines()
-# inserting_services()
+
+cur.execute("DELETE FROM patient;")
+cur.execute("DELETE FROM doctor;")
+cur.execute("DELETE FROM nurse;")
+cur.execute("DELETE FROM accountant;")
+cur.execute("DELETE FROM receptionist;")
+cur.execute("DELETE FROM storekeeper;")
+cur.execute("DELETE FROM person")
+inserting_patients()
+inserting_doctors()
+inserting_nurses()
+inserting_accountants()
+inserting_receptionists()
+inserting_storekeepers()
+inserting_medicines()
+inserting_services()
 # inserting_appointments()
-inserting_qualifications()
+# inserting_qualifications()
 
 # Make the changes to the database persistent
 con.commit()
--- queries_checker.py
+++ queries_checker.py
@@ -7,7 +7,7 @@ con = psycopg2.connect(
 	user = "postgres",
 	password = "postgres",
 	host = "localhost",
-	port = "60118"
+	port = "60631"
 )
 
 cur = con.cursor()
@@ -22,9 +22,23 @@ fake = Faker()
 # medic_id = int(cur.fetchone()[0])
 # print(medic_id)
 
-cur.execute("select person_id from medic;")
-arr_medic_id = cur.fetchall()
-print(arr_medic_id)
+# cur.execute("select person_id from medic;")
+# arr_medic_id = cur.fetchall()
+# print(arr_medic_id)
+
+cur.execute("delete from person;")
+ssn, address, name, surname, date_of_birth, sex = \
+            fake.itin(), fake.address(), fake.first_name_male(), \
+            fake.last_name_male(), fake.date_of_birth(tzinfo=None, minimum_age=15, maximum_age=115), 'm'
+
+address = address.replace("\n", ", ")
+query = "INSERT INTO person (ssn, address, name, surname, date_of_birth, sex) \
+            VALUES ('%s', '%s', '%s', '%s', '%s', '%s') RETURNING person_id;" % \
+		(ssn, address, name, surname, date_of_birth, sex)
+cur.execute(query)
+value = cur.fetchone()[0]
+print(value)
+
 
 # Make the changes to the database persistent
 con.commit()
